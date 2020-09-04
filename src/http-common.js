5736b28e90c2127a2208725492117ed67ef25384
Carlos A. Minafra Jr <carlos.minafrajr@gmail.com>
2020-09-04 19:37:37 -0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://controle-notas-api.herokuapp.com/',
+  baseURL: 'https://controle-notas-api.herokuapp.com/grade',
   headers: {
     'Content-type': 'application/json',
   },
